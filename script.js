a0a678a2735fd8cfd27f24409c7c0dfbcda55966
https://github.com/dusanivkovic <ivdule77@gmail.com>
2023-03-10 17:44:41 +0100

    Adding card and function

--- script.js
+++ script.js
@@ -12,19 +12,68 @@ let addEmployee = document.querySelector('#employee');
 let modalEmployee = document.querySelector('#modal-employee');
 let addTask = document.querySelector('#task');
 let modalTask = document.querySelector('#modal-task');
-let addBtn = document.querySelectorAll('.btn');
-addEmployee.addEventListener('click', () => openModal(modalEmployee));
+let btnCreateEmployee = document.querySelector('#createEmployee');
+let btnCreateTask = document.querySelector('#createTask');
 let modal = document.querySelectorAll('.modal');
+addEmployee.addEventListener('click', () => openModal(modalEmployee));
 addTask.addEventListener('click', () => openModal(modalTask));
+
 function openModal (id) {
     id.style.display = 'block';
 }
 modal.forEach(item => {
     item.addEventListener('click', (e) => closeModal(e))
 })
+
 function closeModal (id) {
     if (id.target == modalEmployee || id.target == modalTask) {
         id.target.style.display = 'none';
     }
 }
-console.log(modal)
+
+btnCreateEmployee.addEventListener('click', () => {
+    modalEmployee.style.display = 'none';
+    createCard('employee');
+})
+btnCreateTask.addEventListener('click', () => {
+    modalTask.style.display = 'none';
+    createCard('task');
+})
+
+function createCard (role) {
+    const card = document.createElement('div');
+    const cardHeader = document.createElement('div');
+    const cardBody = document.createElement('div');
+    const settings = document.createElement('div');
+    settings.setAttribute('class', 'settings');
+    settings.innerHTML = '<span class="fas fa-user-edit">';
+    card.setAttribute('class','card');
+    cardHeader.setAttribute('class', 'card-header');
+    cardHeader.innerHTML = `<h5>${role}</h5><span class="close">X</span>`;
+    cardBody.setAttribute('class', 'card-body');
+    cardBody.append(settings);
+    card.append(cardHeader, cardBody);
+    document.querySelector('.main').appendChild(card);
+}
+// function createTask () {
+//     const card = document.createElement('div');
+//     const cardHeader = document.createElement('div');
+//     const cardBody = document.createElement('div');
+//     const settings = document.createElement('div');
+//     settings.setAttribute('class', 'settings');
+//     settings.innerHTML = '<span class="fas fa-user-edit">';
+//     card.setAttribute('class','card');
+//     cardHeader.setAttribute('class', 'card-header');
+//     cardHeader.innerHTML = '<h5>task</h5><span class="close">X</span>';
+//     cardBody.setAttribute('class', 'card-body');
+//     cardBody.append(settings);
+//     card.append(cardHeader, cardBody);
+//     document.querySelector('.main').appendChild(card);
+// }
+
+let updateUser = document.querySelectorAll('.fa-user-edit'); 
+updateUser.forEach(item => {
+    item.addEventListener('click', () => {
+        openModal(modalEmployee);
+    })
+})
